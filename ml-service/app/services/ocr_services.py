e8a43a43f6de7c3835e903a26ea5271655113e38
Tara Kresna <nandayavanets@mail.ru>
2026-02-08 22:12:40 +0700

    refactor: update OCR processing logic for improved image handling and text extraction

--- ml-service/app/services/ocr_services.py
+++ ml-service/app/services/ocr_services.py
@@ -4,30 +4,56 @@ import numpy as np
 # from PIL import Image, ImageOps, ImageEnhance
 import asyncio
 import logging
-
+# from paddleocr import PaddleOCR
 logger = logging.getLogger(__name__)
+
+# ocr = PaddleOCR(use_angle_cls=True, lang='en')
+
 async def ocr_image(image_path: str) -> str:
     try:
         loop = asyncio.get_event_loop()
 
         def process():
-            img = cv2.imread(image_path)
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-            img_main = cv2.min(cv2.min(img[:, :, 0], img[:, :, 1]), img[:, :, 2])
-            blackhat = cv2.morphologyEx(img_main, cv2.MORPH_BLACKHAT, kernel)
-            res = cv2.subtract(img_main, blackhat)
-            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-            enhanced = clahe.apply(res)
-            enhanced = cv2.resize(enhanced, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-            denoised = cv2.fastNlMeansDenoising(enhanced, h=10)
-            thresh = cv2.adaptiveThreshold(denoised, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 15)
-            thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-            config = '--psm 4 --oem 3 --dpi 300'
-            return pytesseract.image_to_string(thresh, lang='ind+eng', config=config)
-        
+           img = cv2.imread(image_path)
+           if img is None: return ""
+           img = cv2.resize(img, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+           
+           gray = img[:, :, 1]
+           alpha = 1.8 # Contrast
+           beta = -60  # Brightness
+           adjusted = cv2.convertScaleAbs(gray, alpha=alpha, beta=beta)
+           _, thresh = cv2.threshold(adjusted, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+           config = '--psm 6 --oem 3'
+           return pytesseract.image_to_string(thresh, lang='ind+eng', config=config)
+       
         logger.info(f"Starting OCR processing for image: {image_path}")
         raw_text = await loop.run_in_executor(None, process)
         return raw_text.strip()
     except Exception as e:
         logger.error(f"OCR failed for image {image_path}: {e}")
         return ""
+
+
+# def ocr_image(img_path):
+#     try:
+#         result = ocr.predict(img_path, cls=True)
+        
+#         # Check kalo result kosong atau None
+#         if not result or result[0] is None:
+#             return ""
+
+#         lines = []
+#         for res in result:
+#             for line in res:
+#                 text = line[1][0] # Ambil teksnya
+#                 confidence = line[1][1] # Ambil tingkat keyakinannya
+                
+#                 # Filter: kalo confidence di bawah 0.5, skip aja biar gak jadi sampah ke AI
+#                 if confidence > 0.5:
+#                     lines.append(text)
+
+#         return " ".join(lines)
+    
+#     except Exception as e:
+#         print(f"PaddleOCR Error: {e}")
+#         return ""
