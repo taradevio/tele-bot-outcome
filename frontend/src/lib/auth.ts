56df79d2e93052172eee7d9fc07c2d736a2ff5d8
Tara Kresna <nandayavanets@mail.ru>
2026-02-23 14:28:39 +0700

    updated: encrypt and decrypt for jwt token

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,15 +1,19 @@
+import { encrypt, decrypt } from "./crypto";
+
 let _accessToken: string | null = null;
 
-export const setToken = (token: string) => {
+export const setToken = async (token: string) => {
   _accessToken = token;
-  sessionStorage.setItem("access_token", token);
+  const encrypted = await encrypt(token)
+  sessionStorage.setItem("access_token", encrypted);
 };
-export const getToken = (): string | null => {
+export const getToken = async (): Promise<string | null> => {
   if (_accessToken) return _accessToken;
   const stored = sessionStorage.getItem("access_token");
   if (stored) {
     _accessToken = stored;
-    return stored;
+    const decrypted = await decrypt(stored)
+    return decrypted;
   }
   return null;
 };
